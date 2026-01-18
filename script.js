5f34effa3bd98bd53254eeb5fbcb22b7a7acb52f
Nicolás Olivares <142279848+neurott@users.noreply.github.com>
2026-01-18 16:57:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ const paginas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 
 paginas.forEach(num => {
     const img = document.createElement('img');
 
-    img.src = `DOSSIER/PAG${num}.jpg`; 
+    img.src = "./DOSSIER/PAG" + i + ".jpg"; 
     img.className = 'dossier-page';
     img.alt = `Página ${num}`;
     img.loading = "lazy";
     
     gallery.appendChild(img);
 });
+
